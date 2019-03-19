047eb75a9de7666c847ed7d37c09d62bd08fb390
pavelm1997 <34282964+pavelm1997@users.noreply.github.com>
2019-03-19 20:34:47 +0200

    lesson23

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App=()=>{
 
           <div className='app-wrapper-content'>
 
-              <Route path='/dialogs' component={Dialogs}/>
+              <Route  path='/dialogs' component={Dialogs}/>
               <Route path='/profile' component={Profile}/>
               <Route path='/news' component={News}/>
               <Route path='/music' component={Music}/>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 const Dialogs=(props)=>{
     return( <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog+' '+s.active}>
-                eugen
+                <NavLink to='/dialogs/1'>Eugen</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Pavel
+                    <NavLink to='/dialogs/2'>Pavel</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'> Oleks</NavLink>
                 </div>
             </div>
             <div className={s.messeges}>
