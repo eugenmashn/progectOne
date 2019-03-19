9bf29329835f1fe3b563991cdaf931ba72f128eb
pavelm1997 <34282964+pavelm1997@users.noreply.github.com>
2019-03-19 20:15:53 +0200

    lesson21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import s from './Dialogs.module.css'
 const Dialogs=(props)=>{
-    return( <div>
-            Dialogs
+    return( <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog+' '+s.active}>
+                eugen
+                </div>
+                <div className={s.dialog}>
+                    Pavel
+                </div>
+            </div>
+            <div className={s.messeges}>
+                <div className={s.messege}> Hi</div>
+                <div className={s.messege}>How are your</div>
+                <div className={s.messege}>Ok it is very good</div>
+
+            </div>
         </div>
     )
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,11 +14,11 @@ const Navbar=()=>{
                 <NavLink to='/news' activeClassName={s.activeLink}> News</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to='music' activeClassName={s.activeLink}> Music</NavLink>
+                <NavLink to='/music' activeClassName={s.activeLink}> Music</NavLink>
 
             </div>
             <div className={s.item}>
-                <NavLink to='settings' activeClassName={s.activeLink}> Settings</NavLink>
+                <NavLink to='/settings' activeClassName={s.activeLink}> Settings</NavLink>
             </div>
         </nav>
     )
